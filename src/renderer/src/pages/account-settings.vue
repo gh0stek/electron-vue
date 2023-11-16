5a29658df335781ad06a283daff875d42cb70a6c
Bartek Bielicki <bartekbielicki@gmail.com>
2023-11-16 09:13:34 +0100

    lint

--- src/renderer/src/pages/account-settings.vue
+++ src/renderer/src/pages/account-settings.vue
@@ -12,36 +12,21 @@ const activeTab = ref(route.params.tab)
 const tabs = [
   { title: 'Account', icon: 'mdi-account-outline', tab: 'account' },
   { title: 'Security', icon: 'mdi-lock-open-outline', tab: 'security' },
-  { title: 'Notifications', icon: 'mdi-bell-outline', tab: 'notification' },
+  { title: 'Notifications', icon: 'mdi-bell-outline', tab: 'notification' }
 ]
 </script>
 
 <template>
   <div>
-    <VTabs
-      v-model="activeTab"
-      show-arrows
-    >
-      <VTab
-        v-for="item in tabs"
-        :key="item.icon"
-        :value="item.tab"
-      >
-        <VIcon
-          size="20"
-          start
-          :icon="item.icon"
-        />
+    <VTabs v-model="activeTab" show-arrows>
+      <VTab v-for="item in tabs" :key="item.icon" :value="item.tab">
+        <VIcon size="20" start :icon="item.icon" />
         {{ item.title }}
       </VTab>
     </VTabs>
     <VDivider />
 
-    <VWindow
-      v-model="activeTab"
-      class="mt-5 disable-tab-transition"
-      :touch="false"
-    >
+    <VWindow v-model="activeTab" class="mt-5 disable-tab-transition" :touch="false">
       <!-- Account -->
       <VWindowItem value="account">
         <AccountSettingsAccount />
